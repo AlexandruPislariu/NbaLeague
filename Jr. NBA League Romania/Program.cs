bd712420eedaea2e28f218050d5191420d3ed66f
pislaBos <57287326+pislaBos@users.noreply.github.com>
2020-12-21 11:37:07 +0200

    Add project files.

---
+++ Jr. NBA League Romania/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Npgsql;
+
+namespace Jr._NBA_League_Romania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
