95243f82ee6c40611054bf4364be9182cb594e89
pislaBos <57287326+pislaBos@users.noreply.github.com>
2020-12-21 12:33:21 +0200

    Set Up App

---
+++ Jr. NBA League Romania/repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Jr._NBA_League_Romania.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jr._NBA_League_Romania.repository
+{
+    interface IRepository<ID, E> where E : Entity<ID>
+    {
+        E FindOne(ID id);
+        IEnumerable<E> FindAll();
+        E Save(E entity);
+        E Delete(ID id);
+        E Update(E entity);
+    }
+}
